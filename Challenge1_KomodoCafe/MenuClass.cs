77af01e4a4b0037aa7765e13c78d2086f721e684
RoyDBentz <roydbentz@gmail.com>
2021-08-20 12:06:29 -0400

    Try 2

--- Challenge1_KomodoCafe/MenuClass.cs
+++ Challenge1_KomodoCafe/MenuClass.cs
@@ -8,31 +8,6 @@ namespace Challenge1_KomodoCafe
 {
     class MenuClass
     {
-        private int _calories;
-        public int Calories
-        {
-            get { return _calories; }
-            set { _calories = value; }
-        }
-        public string MealName { get; set; }
-        private double _price;
-        public Double Price
-        {
-            get { return _price; }
-            set { _price = value; }
-        }
-        public string Description { get; set; }
-        public string MealNumber { get; set; }
-        public string Ingredients { get; set; }
 
-        public MenuClass(string mealName, string description, string mealNumber, string ingredients, int calories, double price)
-        {
-            MealName = mealName;
-            Calories = calories;
-            Description = description;
-            MealNumber = mealNumber;
-            Ingredients = ingredients;
-            Price = price;
-        }
     }
 }
--- Challenge1_KomodoCafe/MenuRepository.cs
+++ Challenge1_KomodoCafe/MenuRepository.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Challenge1_KomodoCafe
 {
-    class MenuRepository
+    public class MenuItem
     {
+        private int _calories;
+        public int Calories
+        {
+            get { return _calories; }
+            set { _calories = value; }
+        }
+        public string MealName { get; set; }
+        private double _price;
+        public Double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public string Description { get; set; }
+        public string MealNumber { get; set; }
+        public string Ingredients { get; set; }
+
+        public MenuItem() { }   // Empty constructor
+        public MenuItem(string mealNumber, string mealName, string description, string ingredients, double price, int calories)
+        {
+            MealName = mealName;
+            Calories = calories;
+            Description = description;
+            MealNumber = mealNumber;
+            Ingredients = ingredients;
+            Price = price;
+        }
     }
 }
