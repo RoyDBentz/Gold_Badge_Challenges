9103609654ade2c14741ce383e92e7556ffd001f
RoyDBentz <roydbentz@gmail.com>
2021-08-20 10:42:43 -0400

    Forgot to SAVE menu class changes FIRST

--- Challenge1_KomodoCafe/CafeUnitTest.cs
+++ Challenge1_KomodoCafe/CafeUnitTest.cs
@@ -4,7 +4,7 @@ using System;
 namespace Challenge1_KomodoCafe
 {
     [TestClass]
-    public class UnitTest1
+    public class CafeUnitTest
     {
         [TestMethod]
         public void TestMethod1()
--- Challenge1_KomodoCafe/MenuClass.cs
+++ Challenge1_KomodoCafe/MenuClass.cs
@@ -8,5 +8,31 @@ namespace Challenge1_KomodoCafe
 {
     class MenuClass
     {
+        private int _calories;
+        public int Calories
+        {
+            get { return _calories; }
+            set { _calories = value; }
+        }
+        public string Name { get; set; }
+        private double _price;
+        public Double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public string Description { get; set; }
+        public string MealNumber { get; set; }
+        public string Ingredients { get; set; }
+
+        public MenuClass(string name, string description, string mealNumber, string ingredients, int calories, double price)
+        {
+            Name = name;
+            Calories = calories;
+            Description = description;
+            MealNumber = mealNumber;
+            Ingredients = ingredients;
+            Price = price;
+        }
     }
 }
